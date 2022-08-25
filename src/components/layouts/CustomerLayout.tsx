1dcaca9ce555f555a56703e7fc0e1925a159799e
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-08-25 08:37:19 +0700

    [update] - update customer item

--- src/components/layouts/CustomerLayout.tsx
+++ src/components/layouts/CustomerLayout.tsx
@@ -3,18 +3,24 @@ import useWeb from 'hooks/useWeb'
 import { forwardRef } from 'react'
 import { CustomListContainer } from 'styles'
 import { dateFormat } from 'utils'
+import LocalPhoneRoundedIcon from '@mui/icons-material/LocalPhoneRounded'
+import CakeRoundedIcon from '@mui/icons-material/CakeRounded'
+import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded'
 
 const Item = (props, ref) => {
-  const { name, phone, dateOfBirth, action } =
+  const { name, phone, dateOfBirth, address, action } =
     props
   return (
     <div className='list-item' ref={ref}>
-      <div className="content" style={{ display: 'flex', flexDirection: 'column', flex: '0 50%' }}>
-        <span>{name}</span>
-        <span className="detail">{phone}</span>
-      </div>
-      <div className="content" style={{ flex: '0 50%' }}>
-        {dateFormat(dateOfBirth)} 
+      <div className='content' style={{ display: 'flex', flexDirection: 'column', flex: '0 100%', marginRight: 10 }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <span>{name}</span>
+          <span className="detail" style={{ display: 'flex', alignItems: 'center' }}><LocalPhoneRoundedIcon style={{ fontSize: 15, marginRight: 5 }} /><span>{phone}</span></span>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div className="detail" style={{ display: 'flex', alignItems: 'end' }}><LocationOnRoundedIcon style={{ fontSize: 16, marginRight: 5 }} />{address}</div>
+          <div className="detail" style={{ display: 'flex', alignItems: 'end' }}><CakeRoundedIcon style={{ fontSize: 18, marginRight: 5 }} />{dateFormat(dateOfBirth)}</div>
+        </div>
       </div>
       <div className='action' style={{ position: 'absolute', right: 20 }}>
         {action}
