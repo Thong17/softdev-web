f1d4eb7111082c4cf592e492005eb0ab2a1e7691
SATHAPANA\chhan.bunthong <chhan.bunthong@sathapana.com.kh>
2022-11-27 09:25:48 +0700

    [update] - reverse all transaction

--- src/modules/sale/transaction/Header.tsx
+++ src/modules/sale/transaction/Header.tsx
@@ -1,12 +1,25 @@
 import SaleBreadcrumbs from '../components/Breadcrumbs'
 import { useState } from 'react'
-import { MenuItem } from '@mui/material'
+import { IconButton, MenuItem } from '@mui/material'
 import { SortIcon } from 'components/shared/icons/SortIcon'
 import useLanguage from 'hooks/useLanguage'
 import { FilterButton } from 'components/shared/table/FilterButton'
 import { SearchField } from 'components/shared/table/SearchField'
+import useTheme from 'hooks/useTheme'
+import RestartAltRoundedIcon from '@mui/icons-material/RestartAltRounded'
+import { CustomHeaderButton } from 'styles/index'
+import Axios from 'constants/functions/Axios'
+import useNotify from 'hooks/useNotify'
+import useAlert from 'hooks/useAlert'
+import { useAppDispatch } from 'app/hooks'
+import { getListTransaction } from './redux'
 
 export const Header = ({ handleSearch, handleFilter }) => {
+  const { theme } = useTheme()
+  const { notify } = useNotify()
+  const confirm = useAlert()
+  const dispatch = useAppDispatch()
+  
   const [sortObj, setSortObj] = useState({
     description: false,
     createdAt: false,
@@ -33,6 +46,26 @@ export const Header = ({ handleSearch, handleFilter }) => {
     )
   }
 
+  const handleReverseAll = () => {
+    confirm({
+      title: 'Are you sure you want to reverse all transaction?',
+      description: 'Reverse all transaction will update all stock product.',
+      variant: 'error'
+    })
+      .then(() => {
+        Axios({
+          method: 'DELETE',
+          url: '/sale/transaction/reverseAll',
+        })
+          .then(data => {
+            notify(data?.data?.msg, 'success')
+            dispatch(getListTransaction({}))
+          })
+          .catch(err => notify(err?.response?.data?.msg))
+      })
+      .catch()
+  }
+
   return (
     <>
       <SaleBreadcrumbs page='transaction' />
@@ -41,6 +74,9 @@ export const Header = ({ handleSearch, handleFilter }) => {
         <FilterButton style={{ marginLeft: 10 }}>
           <FilterOption />
         </FilterButton>
+        <CustomHeaderButton onClick={handleReverseAll} styled={theme} style={{ marginLeft: 10 }}>
+          <IconButton><RestartAltRoundedIcon /></IconButton>
+        </CustomHeaderButton>
       </div>
     </>
   )
