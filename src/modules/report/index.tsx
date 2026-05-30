9ed9994e8519abdd82d2716ab7bf3c5109979420
Thong17 <chhanbunthong@gmail.com>
2026-05-30 10:39:20 +0700

    update: permission

--- src/modules/report/index.tsx
+++ src/modules/report/index.tsx
@@ -3,16 +3,20 @@ import ReportNavbar from './components/ReportNavbar'
 import { useOutlet } from 'react-router'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import useAuth from 'hooks/useAuth'
 
 export const Report = () => {
   const outlet = useOutlet()
   const location = useLocation()
   const navigate = useNavigate()
+  const { user } = useAuth()
   
   useEffect(() => {
-    if (location.pathname === '/report') navigate('/report/sale')
-  }, [location.pathname, navigate])
-  
+    if (location.pathname === '/report') {
+      user?.privilege?.report?.sale ? navigate('/report/sale') : navigate('/report/payment')
+    }
+  }, [location.pathname, navigate, user?.privilege?.report?.sale])
+
   return (
     <Layout navbar={<ReportNavbar />}>
       {outlet}
